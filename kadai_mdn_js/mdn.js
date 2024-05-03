84824e8567668f1985574397ba7a9dac0d375107
yashiro846846 <168149360+yashiro846846@users.noreply.github.com>
2024-05-03 22:11:08 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-const year = today.getFullYear();
-const month = today.getMonth() + 1;
-const date = today.getDate();
-
-console.log(today.getFullYear() + '年' + month + '月' + date + '日');
